47fcf025e8289c4de25b5e16de30e5c51b341277
nosefijate <92641891+nosefijate@users.noreply.github.com>
2022-02-07 23:20:58 -0300

    firebase

--- src/componentes/Firebase.js
+++ src/componentes/Firebase.js
@@ -1,14 +1,21 @@
-import { initializeApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDZWAyfJRcFNxCfoQfEjIn2VlufWh0bIgw",
-    authDomain: "ecommerce-llera.firebaseapp.com",
-    projectId: "ecommerce-llera",
-    storageBucket: "ecommerce-llera.appspot.com",
-    messagingSenderId: "452830003847",
-    appId: "1:452830003847:web:7d998377fd6f2566a22e22",
-    measurementId: "G-ZK1TT9YDQ2"
-  }
-  const app = initializeApp(firebaseConfig)
-  export const db = getFirestore(app)
+  apiKey: "AIzaSyDZWAyfJRcFNxCfoQfEjIn2VlufWh0bIgw",
+  authDomain: "ecommerce-llera.firebaseapp.com",
+  projectId: "ecommerce-llera",
+  storageBucket: "ecommerce-llera.appspot.com",
+  messagingSenderId: "452830003847",
+  appId: "1:452830003847:web:cf6ea8b35997c4eda22e22",
+  measurementId: "G-GWS69FEKQ7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
